43d82b48706a6e5a3a78c0690f45f909453832b5
Natthawat Narin <natthawat.narin@gmail.com>
2025-03-12 12:12:18 +0700

    *

--- src/main/kotlin/com/ktb/dcb/report/service/GenerateReportService.kt
+++ src/main/kotlin/com/ktb/dcb/report/service/GenerateReportService.kt
@@ -1,5 +1,6 @@
 package com.ktb.dcb.report.service
 
+import com.ktb.dcb.report.model.request.ReceiptReport
 import net.sf.jasperreports.engine.JasperExportManager
 import net.sf.jasperreports.engine.JasperFillManager
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource
@@ -7,26 +8,24 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
-import com.ktb.dcb.report.model.request.ReceiptReport
 
 @Service
 class GenerateReportService() {
+        fun exec(receiptDetail: ReceiptReport): ResponseEntity<ByteArray> {
+                val jasperStream =
+                        this::class.java.getResourceAsStream("/reports/receipt_template.jasper")
+                                ?: throw IllegalArgumentException("Report template not found")
 
-    fun exec(receiptDetail: ReceiptReport): ResponseEntity<ByteArray> {
-        val jasperStream =
-                this::class.java.getResourceAsStream("/reports/receipt_template.jasper")
-                        ?: throw IllegalArgumentException("Report template not found")
+                val dataSource = JRBeanCollectionDataSource(listOf(receiptDetail))
+                val parameters = mutableMapOf<String, Any>()
 
-        val dataSource = JRBeanCollectionDataSource(listOf(receiptDetail))
-        val parameters = mutableMapOf<String, Any>()
+                val jasperPrint = JasperFillManager.fillReport(jasperStream, parameters, dataSource)
 
-        val jasperPrint = JasperFillManager.fillReport(jasperStream, parameters, dataSource)
+                val pdfBytes: ByteArray = JasperExportManager.exportReportToPdf(jasperPrint)
 
-        val pdfBytes: ByteArray = JasperExportManager.exportReportToPdf(jasperPrint)
-
-        return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=billing.pdf")
-                .contentType(MediaType.APPLICATION_PDF)
-                .body(pdfBytes)
-    }
+                return ResponseEntity.ok()
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=billing.pdf")
+                        .contentType(MediaType.APPLICATION_PDF)
+                        .body(pdfBytes)
+        }
 }
